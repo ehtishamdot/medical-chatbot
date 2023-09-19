ad8ca9531a925b45f2fdcc7558cce513358c7295
ehtishamdot <ahtionpc1@gmail.com>
2023-09-19 12:25:24 +0500

    debug to true

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -9,19 +9,7 @@ const handler = NextAuth({
     }),
   ],
   secret: process.env.NEXTAUTH_SECRET,
-  callbacks: {
-    async signIn({ user, account, profile, email, credentials }) {
-      const isAllowedToSignIn = true
-      if (isAllowedToSignIn) {
-        return true
-      } else {
-        // Return false to display a default error message
-        return false
-        // Or you can return a URL to redirect to:
-        // return '/unauthorized'
-      }
-    }
-  }
+  debug:true
 });
 
 export { handler as GET, handler as POST };
