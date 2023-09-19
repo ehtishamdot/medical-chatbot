e32976ca928c2941d8e3445b34385fc88204800d
ehtishamdot <ahtionpc1@gmail.com>
2023-09-19 18:40:47 +0500

    login route in mobile

--- src/app/landing/page.tsx
+++ src/app/landing/page.tsx
@@ -45,9 +45,10 @@ export default function Landing() {
         <div className="flex lg:hidden">
           <button type="button" className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700">
             <span className="sr-only">Open main menu</span>
-            <svg className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
+            {/* <svg className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
               <path strokeLinecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
-            </svg>
+            </svg> */}
+             <Link href={'/auth/login'} className="text-sm font-semibold leading-6 text-gray-900">Gabeth App <span aria-hidden="true">&rarr;</span></Link>
           </button>
         </div>
         {/* <div className="hidden lg:flex lg:gap-x-12">
