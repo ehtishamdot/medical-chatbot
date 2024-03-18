c84a3b61a28a9f8f2a88524d542f62166a22816a
ehtishamdot <ahtionpc1@gmail.com>
2024-03-18 15:36:39 +0500

    chore: dashboard endpoint and feedback endpoint

---
+++ src/app/api/patient/feedback/route.ts
@@ -0,0 +1,75 @@
+import { prisma } from "@/db/config";
+import ServerError, { JWTPayload } from "@/lib/types";
+import { decryptToken, errorHandler, getOpenAIApiInstance } from "@/lib/utils";
+import { deepEqual } from "assert";
+import { NextRequest, NextResponse } from "next/server";
+import { z } from "zod";
+
+export async function POST(req: NextRequest) {
+  try {
+    const { rating, comment, patientId } = await req.json();
+    const authorizationHeader = req.headers.get("Cookie");
+    const refreshTokenStartIndex =
+      authorizationHeader?.match(/refreshToken=([^;]*)/)?.[1];
+    if (!refreshTokenStartIndex) {
+      throw new ServerError("Unauthorized", 401);
+    }
+    const accessToken = refreshTokenStartIndex;
+    const dbToken = await prisma.token.findFirst({
+      where: {
+        token: accessToken,
+      },
+    });
+    if (!dbToken) throw new ServerError("Invalid token provided", 409);
+    const { id } = decryptToken(accessToken, process.env.JWT_REFRESH_SECRET!);
+    const availablePatientStatus = await prisma.patient.findUnique({
+      where: {
+        id: patientId,
+        addedByUserId: id,
+      },
+    });
+
+    if (availablePatientStatus) {
+      const feedback = await prisma.feedback.create({
+        data: {
+          patientId,
+          rating,
+          comment,
+        },
+      });
+      return NextResponse.json(feedback);
+    }
+  } catch (err) {
+    console.error(err);
+    return errorHandler(err);
+  }
+}
+
+export async function GET(req: NextRequest) {
+  try {
+    const authorizationHeader = req.headers.get("Cookie");
+    console.log(authorizationHeader);
+    const refreshTokenStartIndex =
+      authorizationHeader?.match(/refreshToken=([^;]*)/)?.[1];
+    if (!refreshTokenStartIndex) {
+      throw new ServerError("Unauthorized", 401);
+    }
+    const accessToken = refreshTokenStartIndex;
+    const dbToken = await prisma.token.findFirst({
+      where: {
+        token: accessToken,
+      },
+    });
+    if (!dbToken) throw new ServerError("Invalid token provided", 409);
+    const { id } = decryptToken(accessToken, process.env.JWT_REFRESH_SECRET!);
+    const userPatients = await prisma.patient.findMany({
+      where: {
+        addedByUserId: id,
+      },
+    });
+    return NextResponse.json(userPatients);
+  } catch (err) {
+    console.error(err);
+    return errorHandler(err);
+  }
+}
