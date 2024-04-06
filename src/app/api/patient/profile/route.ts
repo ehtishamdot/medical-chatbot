7a820ad8337f5a41e100dd26e542856677ba3a2f
ehtishamdot <ahtionpc1@gmail.com>
2024-04-06 23:16:39 +0500

    chore: profile not fetching in assistant endpoint fixed

--- src/app/api/patient/profile/route.ts
+++ src/app/api/patient/profile/route.ts
@@ -30,9 +30,32 @@ export async function GET(req: NextRequest, res: NextResponse) {
     if (!dbToken) throw new ServerError("Invalid token provided", 409);
 
     const { id } = decryptToken(accessToken, process.env.JWT_REFRESH_SECRET!);
+
+    let result = await prisma.$transaction([
+      prisma.user.findFirst({
+        where: {
+          id,
+        },
+      }),
+      prisma.assistant.findFirst({
+        where: {
+          id,
+        },
+        include: {
+          user: true,
+        },
+      }),
+    ]);
+    let user = result[0] || result[1];
+    let addedByUserId = user?.id;
+    if (user?.role === "ASSISTANT") {
+      addedByUserId = user?.user?.id;
+    } else {
+      addedByUserId = user?.id;
+    }
     const patient = await prisma.patient.findUnique({
       where: {
-        addedByUserId: id,
+        addedByUserId,
         id: patientId,
       },
     });
